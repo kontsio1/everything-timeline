c3e144e36575fd54e16a9cbb43b854a94dd5f4a6
kontsio1 <kontsio18@gmail.com>
2026-05-15 17:17:51 +0100

    initialize MSAL instance and refactor account selection logic

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -8,32 +8,35 @@ import { EventType } from '@azure/msal-browser';
 import {MsalProvider} from "@azure/msal-react";
 import { msalInstance } from "./api/msalInstance";
 
-// Default to using the first account if no account is active on page load
-if (!msalInstance.getActiveAccount() && msalInstance.getAllAccounts().length > 0) {
-    // Account selection logic is app dependent. Adjust as needed for different use cases.
-    msalInstance.setActiveAccount(msalInstance.getAllAccounts()[0]);
-}
-
-// Listen for sign-in event and set active account
-msalInstance.addEventCallback((event: any) => {
-    if (event.eventType === EventType.LOGIN_SUCCESS && event?.payload?.account) {
-        const account = event.payload.account;
-        msalInstance.setActiveAccount(account);
+// Initialize MSAL before using the instance
+msalInstance.initialize().then(() => {
+    // Default to using the first account if no account is active on page load
+    if (!msalInstance.getActiveAccount() && msalInstance.getAllAccounts().length > 0) {
+        // Account selection logic is app dependent. Adjust as needed for different use cases.
+        msalInstance.setActiveAccount(msalInstance.getAllAccounts()[0]);
     }
-});
 
-const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
-);
-root.render(
-    <React.StrictMode>
-        <MsalProvider instance={msalInstance}>
-            <ControlsProvider>
-                <App/>
-            </ControlsProvider>
-        </MsalProvider>
-    </React.StrictMode>
-);
+    // Listen for sign-in event and set active account
+    msalInstance.addEventCallback((event: any) => {
+        if (event.eventType === EventType.LOGIN_SUCCESS && event?.payload?.account) {
+            const account = event.payload.account;
+            msalInstance.setActiveAccount(account);
+        }
+    });
+
+    const root = ReactDOM.createRoot(
+        document.getElementById('root') as HTMLElement
+    );
+    root.render(
+        <React.StrictMode>
+            <MsalProvider instance={msalInstance}>
+                <ControlsProvider>
+                    <App/>
+                </ControlsProvider>
+            </MsalProvider>
+        </React.StrictMode>
+    );
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
